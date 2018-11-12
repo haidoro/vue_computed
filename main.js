d9a7a3d894607488e29f972cd457854f3b3a106e
Tahara <hydoro-3181@itstudio.co>
2018-11-12 17:33:14 +0900

    computed活用

--- main.js
+++ main.js
@@ -2,15 +2,11 @@ var vm = new Vue({
   el: '#demo',
   data: {
     firstName: 'Foo',
-    lastName: 'Bar',
-    fullName: 'Foo Bar'
+    lastName: 'Bar'
   },
-  watch: {
-    firstName: function (val) {
-      this.fullName = val + ' ' + this.lastName
-    },
-    lastName: function (val) {
-      this.fullName = this.firstName + ' ' + val
+  computed: {
+    fullName: function () {
+      return this.firstName + ' ' + this.lastName
     }
   }
 })
