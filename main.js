f71d1bec2c2e9ba49a9af784cb74242d450d13c1
Tahara <hydoro-3181@itstudio.co>
2018-11-12 14:22:52 +0900

    computed sample

---
+++ main.js
@@ -0,0 +1,17 @@
+var vm = new Vue({
+  el: '#example',
+  data: {
+    message: 'Hello'
+  },
+  computed: {
+    // 算出 getter 関数
+    reversedMessage: function () {
+      // `this` は vm インスタンスを指します
+      return this.message.split('').reverse().join('')
+    }
+  }
+})
+
+console.log(vm.reversedMessage) // => 'olleH'
+vm.message = 'Tahara'
+console.log(vm.reversedMessage) // => 'eybdooG'
